75050266ba4243ed7176cce66561ddcfb646bf3c
vadim <skyjoeee@gmail.com>
2020-12-01 20:46:27 +0200

    add index file

---
+++ js/scrtipt.js
@@ -0,0 +1,19 @@
+console.log(10 + +"10");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(2 * 4 == 8);
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
+
+console.log(2 + 2 * 2 !== 8);
