61146915650c4d353824e82e9d9d07200c0281e4
vadim <skyjoeee@gmail.com>
2020-12-02 12:40:03 +0200

    add if/else lesson

--- js/scrtipt.js
+++ js/scrtipt.js
@@ -17,3 +17,37 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+
+if (4 == 4) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+
+
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
+
+const num = 50;
+switch (num) {
+    case 49:
+        console.log('False');
+        break;
+    case 100:
+        console.log('False');
+        break;
+    case 50:
+        console.log(' It`s True!');
+        break;
+    default:
+        console.log('Not today!');
+        break;
+}
