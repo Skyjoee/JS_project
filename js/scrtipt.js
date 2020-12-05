b59941935b4f4bd332050b6a002a385908591f3d
vadim <skyjoeee@gmail.com>
2020-12-05 11:10:22 +0200

    add 1 project

--- js/scrtipt.js
+++ js/scrtipt.js
@@ -1,29 +1,71 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+"use strict";
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    genrest: [],
+    genres: [],
     privat: false
 };
 
 const a = prompt('Один из просмотренных фильмов?', ''),
-    b = prompt('На с колько оцените его?', ''),
-    c = prompt('Один из просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
-
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+       
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
 
 
-if (4 == 4) {
-    console.log('true');
-} else {
-    console.log('false');
-}
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genrest: [],
+//     privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+    
+// }
+
+// if (personalMovieDB.count < 10) {
+//     console.log("Просмотрено довольно мало фильмов")
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log("Вы классический зритель");
+// } else if (personalMovieDB.count >= 30) {
+//     console.log("Вы киноман");
+// } else {
+//     console.log("Произошла ошибка");
+// }
+
+// console.log(personalMovieDB);
+
+
+
+
+// if (4 == 4) {
+//     console.log('true');
+// } else {
+//     console.log('false');
+// }
 
 
 
@@ -36,18 +78,41 @@ if (4 == 4) {
 //     console.log('Ok!');
 // }
 
-const num = 50;
-switch (num) {
-    case 49:
-        console.log('False');
-        break;
-    case 100:
-        console.log('False');
-        break;
-    case 50:
-        console.log(' It`s True!');
-        break;
-    default:
-        console.log('Not today!');
-        break;
-}
+// const num = 50;
+// switch (num) {
+//     case 49:
+//         console.log('False');
+//         break;
+//     case 100:
+//         console.log('False');
+//         break;
+//     case 50:
+//         console.log(' It`s True!');
+//         break;
+//     default:
+//         console.log('Not today!');
+//         break;
+// }
+
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+
+// while (num < 55);
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+
+//     console.log(i);
+// }
